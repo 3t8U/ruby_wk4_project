77c85b36401e0252f9e41cf0d9de6941b69d3733
Student <example@example.com>
2020-01-17 14:44:15 -0800

    add html & views pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,7 +13,7 @@ class ProductssController < ApplicationController
   def create
     @product = Products.new(product_params)
     if @product.save
-      flash[:notice] = "Album successfully added!"
+      flash[:notice] = "Product successfully added!"
       redirect_to products_path
     else
       render :new
@@ -33,6 +33,7 @@ class ProductssController < ApplicationController
   def update
     @product= Products.find(params[:id])
     if @product.update(product_params)
+      flash[:notice] = "Product successfully updated!"
       redirect_to products_path
     else
       render :edit
@@ -42,12 +43,13 @@ class ProductssController < ApplicationController
   def destroy
     @product = Products.find(params[:id])
     @product.destroy
+    flash[:notice] = "Product successfully deleted!"
     redirect_to products_path
   end
 
   private
     def product_params
-      params.require(:product).permit(:proj_name, :cost, :country_of_origin)
+      params.require(:product).permit(:prod_name, :cost, :country_of_origin)
     end
 
 end
