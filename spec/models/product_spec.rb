0946a3925645be47fc8377c100903f1d01cb9504
Student <example@example.com>
2020-01-17 13:46:58 -0800

    contiue to work on testing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,13 +6,10 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
 
-  it("titleizes the name of a product") do
-    product = Product.create({prod_name: "whetted wipes", cost: 5, country_of_origin: "Guam"})
-    expect(product.cost()).to(eq(5))
-  end
+
 
   it("titleizes the name of a product") do
     product = Product.create({prod_name: "whetted wipes"})
-    expect(product.prod_name()).to(eq("Whetted wipes"))
+    expect(product.prod_name()).to(eq("Whetted Wipes"))
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,10 +5,18 @@ describe Review do
   it { should validate_presence_of :author }
   it { should validate_presence_of :rating }
   it { should validate_presence_of :content_body }
-  it { should validate_length_of(:content_body).is_at_least(50) }
-  it { should validate_length_of(:content_body).is_at_most(250) }
+  it { should validate_length_of (:content_body ).is_at_least(50) }
+  it { should validate_length_of (:content_body ).is_at_most(250) }
   it { should validate_length_of(:rating).is_at_least(1) }
   it { should validate_length_of(:rating).is_at_most(5) }
+
+  it("validate the length of the content body") do
+    product = Product.create({content_body: "whetted wipes dsfkjsdksdjf ksfnkfjnf  kjfnas k;jnk; jafakjafafkjn ak jnka;jn ak;fjn k;jnfk;jafna fkjanf a;fkjn k;ajfn;k janfk;janf ;kajfnak; n fk;janf;kjfnf;kjnf ;kjafn ak;j fn kjanf kjafnakfjnaef "})
+    expect(product.content_body()).to(eq("whetted wipes sfkjsdksdjf ksfnkfjnf  kjfnas k;jnk; jafakjafafkjn ak jnka;jn ak;fjn k;jnfk;jafna fkjanf a;fkjn k;ajfn;k janfk;janf ;kajfnak; n fk;janf;kjfnf;kjnf ;kjafn ak;j fn kjanf kjafnakfjnaef"))
+  end
+
+
+
   it("titleizes the name of a review author") do
     product = Product.create({prod_name: 'test'})
     review = product.reviews.create({author: "billy badass"})
