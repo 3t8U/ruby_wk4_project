2dffe746cb6da57d673586a07fbc68fff10a0640
Student <example@example.com>
2020-01-20 16:25:17 -0800

    create functional scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+  scope :usa, -> { where(country_of_origin: "USA") }
+  scope :most_reviews, -> {(
+      select("products.id, products.prod_name, count(reviews.id) as reviews_count")
+      .joins(:reviews)
+      .group("products.prod_name")
+      .order("reviews_count DESC")
+      .limit(3)
+      )}
   has_many:reviews, dependent: :destroy
   validates :prod_name, presence: true
   validates :cost, presence: true
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,9 +7,6 @@ describe Product do
   it { should validate_presence_of :prod_name }
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
-
-
-
   it("titleizes the name of a product") do
     product = Product.create({prod_name: "whetted wipes"})
     expect(product.prod_name()).to(eq("Whetted Wipes"))
