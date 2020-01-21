a38e285c80697da0368029129357c53d41f8cbf7
Student <example@example.com>
2020-01-20 17:14:12 -0800

    add some CSS

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 
 describe Review do
-review = product.reviews.create!({author: "billy badass", rating: 3, content_body: "nstead of passing a block to the scope method, we use the following operator ->. The -> operator is colloquially known as the stabby lambda. It turns a block into an object called a lambda"})
 
   it { should belong_to :product }
   it { should validate_presence_of :author }
@@ -14,6 +13,12 @@ review = product.reviews.create!({author: "billy badass", rating: 3, content_bod
   it { should validate_length_of(:rating).is_at_most(5) }
 
 
+  # it("validates the length of a rating is not more than 5") do
+  #   product = Product.create!({prod_name: 'test', cost: 3, country_of_origin: "USA"})
+  #   review = product.reviews.create!({author: "billy badass", rating: 7, content_body: "instead of passing a block to the scope method, we use the following operator ->. The -> operator is colloquially known as the stabby lambda. It turns a block into an object called a lambda"})
+  #   expect(review.rating.validate_length_of()).to(eq("Clearly, this product has completely changed your life. We get it. Unfortunately, you review can't give the product more than %{count} stars."))
+  # end
+
 
 
 
@@ -23,9 +28,5 @@ review = product.reviews.create!({author: "billy badass", rating: 3, content_bod
     expect(review.author()).to(eq("Billy Badass"))
   end
 
-  # it("titleizes the name of a review author") do
-  #   product = Product.create!({prod_name: 'test', cost: 3, country_of_origin: "USA"})
-  #   review = product.reviews.create!({author: "billy badass", rating: 3, content_body: "nstead of passing a block to the scope method, we use the following operator ->. The -> operator is colloquially known as the stabby lambda. It turns a block into an object called a lambda"})
-  #   expect(review.author()).to(eq("Billy Badass"))
-  # end
+
 end
