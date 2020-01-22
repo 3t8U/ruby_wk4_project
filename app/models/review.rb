4df808592249a9657eb8d05304031494fe33a847
Nate Powers <russellsproutz@gmail.com>
2020-01-21 22:55:53 -0800

    fix all tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,8 @@ class Review < ApplicationRecord
 validates :author, presence: true
 validates :rating, presence: true
 validates :content_body, presence: true
-validates_numericality_of :content_body, greater_than_or_equal_to: 50
-validates_numericality_of :content_body, less_than: 250
+validates_length_of :content_body,in: 50..250,too_short: 'Review must be more than 50 characters'
+validates_length_of :content_body,in: 50..250,too_long: 'Review must be less than 250 characters'
 validates_numericality_of :rating, greater_than_or_equal_to: 1
 validates_numericality_of :rating, less_than: 5
 before_save(:titleize_review)
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,8 +7,8 @@ it { should belong_to :product }
 it { should validate_presence_of :author }
 it { should validate_presence_of :rating }
 it { should validate_presence_of :content_body }
-it { should validate_numericality_of(:content_body).is_greater_than_or_equal_to(50)}
-it {should validate_numericality_of(:content_body).is_less_than(250)}
+it {should validate_length_of(:content_body).is_at_least(50).with_short_message('Review must be more than 50 characters')}
+it {should validate_length_of(:content_body).is_at_most(250).with_long_message('Review must be less than 250 characters')}
 it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(1)}
 it {should validate_numericality_of(:rating).is_less_than(5)}
 
