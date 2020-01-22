613d4ac75c12b5a2381add229f9d13f91147da53
Nate Powers <russellsproutz@gmail.com>
2020-01-21 19:07:33 -0800

    fix most tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,13 +1,13 @@
 class Review < ApplicationRecord
-  before_save(:titleize_review)
   belongs_to :product
-  validates :author, presence: true
-  validates :rating, presence: true
-  validates :content_body, presence: true
-  validates_length_of :content_body, {minimum: 50, maximum: 250, too_short: "Really?!, you've got to do better than that. Your review must contain at least %{count} characters.",
-  too_long: "Are you writing a novel? Your review cannot have more than %{count} characters."}
-  validates_length_of :rating, {minimum: 1, maximum: 5, too_short: "That bad huh?. Sorry but, your review must contain at least %{count} stars.",
-  too_long: "Clearly, this product has completely changed your life. We get it. Unfortunately, you review can't give the product more than %{count} stars."}
+validates :author, presence: true
+validates :rating, presence: true
+validates :content_body, presence: true
+validates_numericality_of :content_body, greater_than_or_equal_to: 50
+validates_numericality_of :content_body, less_than: 250
+validates_numericality_of :rating, greater_than_or_equal_to: 1
+validates_numericality_of :rating, less_than: 5
+before_save(:titleize_review)
 
 
   private
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,21 +3,15 @@ require 'rails_helper'
 
 describe Review do
 
-  it { should belong_to :product }
-  it { should validate_presence_of :author }
-  it { should validate_presence_of :rating }
-  it { should validate_presence_of :content_body }
-  it { should validate_length_of (:content_body ).is_at_least(50) }
-  it { should validate_length_of (:content_body ).is_at_most(250) }
-  it { should validate_length_of(:rating).is_at_least(1) }
-  it { should validate_length_of(:rating).is_at_most(5) }
+it { should belong_to :product }
+it { should validate_presence_of :author }
+it { should validate_presence_of :rating }
+it { should validate_presence_of :content_body }
+it { should validate_numericality_of(:content_body).is_greater_than_or_equal_to(50)}
+it {should validate_numericality_of(:content_body).is_less_than(250)}
+it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(1)}
+it {should validate_numericality_of(:rating).is_less_than(5)}
 
-
-  # it("validates the length of a rating is not more than 5") do
-  #   product = Product.create!({prod_name: 'test', cost: 3, country_of_origin: "USA"})
-  #   review = product.reviews.create!({author: "billy badass", rating: 7, content_body: "instead of passing a block to the scope method, we use the following operator ->. The -> operator is colloquially known as the stabby lambda. It turns a block into an object called a lambda"})
-  #   expect(review.rating.validate_length_of()).to(eq("Clearly, this product has completely changed your life. We get it. Unfortunately, you review can't give the product more than %{count} stars."))
-  # end
 
 
 
